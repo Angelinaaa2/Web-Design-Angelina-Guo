9bd89e54880f2cedbe4c771550f8304ed4bee0ba
Angelinaaa2 <109514294+Angelinaaa2@users.noreply.github.com>
2022-08-17 18:38:52 +1000

    Edit small things

--- Resources/Js/scripts.js
+++ Resources/Js/scripts.js
@@ -9,13 +9,6 @@ function openNav() {
     toggler.onclick = function () {
         navigation.classList.toggle('open');
     }
-
 }
 
 openNav();
-
-//this is for the gallery 
-
-var lightbox = new SimpleLightbox('.gallery a', {
-
-});
